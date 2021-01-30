750f45d7f806cc8de338f6c867890259a0604adb
Happy Chen <2695401775@qq.com>
2021-01-30 17:10:56 +0800

    webpack插件HtmlWebpackPlugin的简单使用

--- src/index.js
+++ src/index.js
@@ -1,40 +1,11 @@
 import _ from 'lodash';
-import Miao from './miao.jpg';
-//import css
-import './css/a.css'
-
-
-import Data from './data.xml';
-import Notes from './data.csv';
-
-import toml from './data.toml';
-import yaml from './data.yaml';
-import json from './data.json5';
-
-console.log(toml.title); // output `TOML Example`
-console.log(toml.owner.name); // output `Tom Preston-Werner`
-
-console.log(yaml.title); // output `YAML Example`
-console.log(yaml.owner.name); // output `Tom Preston-Werner`
-
-console.log(json.title); // output `JSON5 Example`
-console.log(json.owner.name); // output `Tom Preston-Werner`
 
 function component() {
   const element = document.createElement('div');
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  //增加图片资源
-  const myImage = new Image();
-  myImage.src = Miao
-  element.appendChild(myImage);
-
-  console.log(Data);
-  console.log(Notes);
 
   return element;
 
 }
 
 document.body.appendChild(component());
-
-export let DEV_SERVER = 'DEV_SERVER test';
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,7 @@
 const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const toml = require('toml');
-const yaml = require('yamljs');
-const json5 = require('json5');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -12,65 +10,11 @@ module.exports = {
     // filename: '[name].[contenthash].bundle.js',
     // path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    //path: __dirname + '/test'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          // [style-loader](/loaders/style-loader)
-          { loader: 'style-loader' },
-          // [css-loader](/loaders/css-loader)
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true
-            }
-          }
-        ]
-      },
-      // using the built-in Asset Modules of webpack to incorporate our images
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)/i,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.(woff|woff2|eot|tf|otf)$/i,
-        type: 'asset/resource'
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ['csv-loader'],
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.toml$/i,
-        type: 'json',
-        parser: {
-          parse: toml.parse,
-        },
-      },
-      {
-        test: /\.yaml$/i,
-        type: 'json',
-        parser: {
-          parse: yaml.parse,
-        },
-      },
-      {
-        test: /\.json5$/i,
-        type: 'json',
-        parser: {
-          parse: json5.parse,
-        },
-      },
-    ]
+    path: __dirname + '/dist'
   },
   plugins: [
+    new CleanWebpackPlugin(), // 构建前清理 /dist 文件夹
     new HtmlWebpackPlugin({ template: './src/index.html' })
+    // new HtmlWebpackPlugin({ title: '管理输出' })
   ]
 };
