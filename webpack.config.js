901f5a7dab8716aace30280ef5a21fbe84dba70a
Happy Chen <2695401775@qq.com>
2021-01-31 13:57:45 +0800

    webpack中的开发环境

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,24 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  // tell the dev server where to look for files
+  devServer: {
+    contentBase: './dist',
+  },
   entry: './src/index.js',
   output: {
     // filename: 'main.js',
     // filename: '[name].[contenthash].bundle.js',
     // path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    path: __dirname + '/dist'
+    path: __dirname + '/dist',
+    publicPath: '/'
   },
   plugins: [
-    new CleanWebpackPlugin(), // 构建前清理 /dist 文件夹
+    // new CleanWebpackPlugin(), // 构建前清理 /dist 文件夹
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
     new HtmlWebpackPlugin({ template: './src/index.html' })
     // new HtmlWebpackPlugin({ title: '管理输出' })
   ]
