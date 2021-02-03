15336ce83ceb23617b6d04f6ebc8fa4772ea544b
Happy Chen <2695401775@qq.com>
2021-02-03 11:02:54 +0800

    webpack代码分离—入口依赖

--- webpack.config.js
+++ webpack.config.js
@@ -10,14 +10,26 @@ module.exports = {
   devServer: {
     contentBase: './dist',
   },
-  entry: './src/index.js',
+  entry: {
+    shared: 'lodash',
+    index: {
+      import: './src/index.js',
+      dependOn: 'shared',
+    },
+    another: {
+      import: './src/another-module.js',
+      dependOn: 'shared',
+    },
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
   output: {
     // filename: 'main.js',
     // filename: '[name].[contenthash].bundle.js',
     // path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js',
-    path: __dirname + '/dist',
-    publicPath: '/'
+    path: __dirname + '/dist'
   },
   plugins: [
     // new CleanWebpackPlugin(), // 构建前清理 /dist 文件夹
