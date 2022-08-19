613cf432bc0b0e0ed7a1a0336b88787316eb86ca
happychen666 <2695401775@qq.com>
2022-08-19 10:07:04 +0800

    feat: 初始

--- src/globals.js
+++
@@ -1,9 +0,0 @@
-const file = 'happy chen';
-const helpers = {
-    test: function () {
-        console.log('test something');
-    },
-    parse: function () {
-        console.log('parse something');
-    }
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 const path = require('path');
 
@@ -26,59 +25,3 @@ module.exports = {
         publicPath: '/dist'
     }
 }
-=======
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
-module.exports = {
-  mode: 'development',
-  devtool: 'inline-source-map',
-  entry: {
-    polyfills: './src/polyfills',
-    index: './src/index.js',
-  },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  optimization: {
-    moduleIds: 'deterministic',
-    runtimeChunk: 'single',
-    splitChunks: {
-      // 将第三方库(library) （例如 lodash 或 react）提取到单独的 vendor chunk 文件中
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: 'vendors',
-          chunks: 'all',
-        },
-      },
-    },
-  },
-  plugins: [
-    // new CleanWebpackPlugin(), // 构建前清理 /dist 文件夹
-    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
-    new HtmlWebpackPlugin({ template: './src/index.html' }),
-    // new HtmlWebpackPlugin({ title: '管理输出' })
-    new webpack.ProvidePlugin({
-      // _: 'lodash',
-      join: ['lodash', 'join'],
-    })
-  ],
-  module: {
-    rules: [
-      {
-        test: require.resolve('./src/index.js'),
-        use: 'imports-loader?wrapper=window',
-      },
-      {
-        test: require.resolve('./src/globals.js'),
-        use:
-          'exports-loader?type=commonjs&exports=file,multiple|helpers.parse|parse',
-      }
-    ],
-  },
-};
->>>>>>> 17cd1ae1be56d1cbd738730ec0b36152effb666f
