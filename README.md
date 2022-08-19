4c9c6d52e5ec2a5748c5018e307b768b6710b65f
happychen666 <2695401775@qq.com>
2022-08-19 15:25:33 +0800

    init

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-1，资源的输入和输出
-
-相关的配置项context、entry、output。
-在配置打包入口时，context相当于路径前缀，entry是入口文件路径。
-若为单入口，则chunk name不可更改，若为多入口，则必须为每一个chunk指定chunk name。
-当第三方依赖较多时，我们可以用提取vendor的方法将这些模块打包到一个单独的bundle中，以更有效地利用客户端缓存，加快页面渲染速度。
-path和publicPath的区别在于path指定的是资源的输出位置，而publicPath指定的是间接资源的请求位置
+初始项目
--- src/index.js
+++ src/index.js
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
 import addContent from './add-content.js';
 document.write('My first Webpack app.<br />');
 addContent()
-=======
-const { file, parse } = require('./globals.js')
-console.log(file, parse)
-
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = join(['Hello', 'webpack'], ' ');
-
-  // Assume we are in the context of `window`
-  console.log("Hmmm, this probably isn't a great idea...");
-  // console.log(this);
-
-  return element;
-}
-
-document.body.appendChild(component());
-
-fetch('https://jsonplaceholder.typicode.com/users')
-  .then((response) => response.json())
-  .then((json) => {
-    console.log(
-      "We retrieved some data! AND we're confident it will work on a variety of browser distributions."
-    );
-    console.log(json);
-  })
-  .catch((error) =>
-    console.error('Something went wrong when fetching this data: ', error)
-  );
->>>>>>> 17cd1ae1be56d1cbd738730ec0b36152effb666f
