af0598c338262c6bc17d537f86738cf0479e1021
happychen666 <2695401775@qq.com>
2022-08-19 14:53:11 +0800

    多个入口下打包

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
-1，资源的输入和输出
+# 1，资源的输入和输出
 
-相关的配置项context、entry、output。
-在配置打包入口时，context相当于路径前缀，entry是入口文件路径。
-若为单入口，则chunk name不可更改，若为多入口，则必须为每一个chunk指定chunk name。
-当第三方依赖较多时，我们可以用提取vendor的方法将这些模块打包到一个单独的bundle中，以更有效地利用客户端缓存，加快页面渲染速度。
-path和publicPath的区别在于path指定的是资源的输出位置，而publicPath指定的是间接资源的请求位置
+### 多个入口下打包
+使用 html-webpack-plugin 配置多个页面,
+
+[html-webpack-plugin插件使用参数详见](https://blog.csdn.net/hbiao68/article/details/104054932)
--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,16 @@
 
 const path = require('path');
-
 //D:\happychenData\webpack-demo   __driname的默认值是当前工程的根目录
 console.log('oooooo=====', __dirname, path.join(__dirname, './src/'))
+let HtmlWebpackPlugin = require('html-webpack-plugin')
+let { CleanWebpackPlugin } = require('clean-webpack-plugin')// 文件夹清除工具
+
 
 module.exports = {
-    context: path.join(__dirname, './src/'),
+    context: path.join(__dirname),
     entry: {
-        index: './index.js',
-        vendor: ['vuex', 'lodash', 'vue']
+        pageA: './src/pageA.js',
+        pageB: './src/pageB.js'
     },
     // output: {
     //     filename: 'main.js',
@@ -23,5 +25,22 @@ module.exports = {
     },
     devServer: {
         publicPath: '/dist'
-    }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),// 清空dist文件夹
+        // 参数使用详见https://blog.csdn.net/hbiao68/article/details/104054932
+        new HtmlWebpackPlugin({
+            filename: 'pageA.html',  						//打包后文件的名字
+            template: './public/a.html',		//   模板文件的名字
+            chunks: ['pageA'],									// 与entry入口文件属性搭配			配置各自的文件路径 
+            title: '我是a.html的标题',
+
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'pageB.html',  //打包后文件的名字
+            template: './public/b.html',//   模板文件的名字
+            chunks: ['pageB'],
+            title: '我是b.html的标题'
+        })
+    ]
 }
